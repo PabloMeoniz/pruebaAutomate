5c4cac83f22d349c399b63edf5b216608bf636d8
Pablo Meoniz <meoniz.p.o@gmail.com>
2025-10-22 12:51:22 -0300

    vamos a subir test.py y index.html

---
+++ test.py
@@ -0,0 +1,133 @@
+
+#tipos de datos
+
+#str => String => cadena de texto
+#int => Enteros
+#float => Decimales
+#bool => True o False
+
+nombre = "Luis"
+edad = 39
+altura = 1.60
+esMayorDeEdad = True
+#Entrada y salida de datos
+#print(nombre)
+#anioDeNacimiento = int(input("ingresa tu año de nacimiento: "))
+#print(type(anioDeNacimiento))
+#Convertir Tipos de Datos
+resultado = 2 + 3
+potencia = 2**2
+
+division = 10 // 5
+#print(division)
+resto = 4 % 2
+#print(resto)
+result = 5 == 2
+#(edad >= 18) and (licencia == True) => True
+cursarEnOtra = True
+not(edad > 18 or cursarEnOtra)
+
+'''#puntuacion = int(input("ingrese una nota - (0-10)"))
+if puntuacion >= 9:
+    print("Exelente")
+elif puntuacion >= 8:
+    print("bien")
+else:
+    print('desaprobado') 
+'''
+
+#range(stop) 10
+#range(start, stop) 2 - 11
+#range(start, stop, step) 2 - 11 -- 2
+'''
+for i in range(12):
+    if i==5:
+        continue
+    print(i)
+'''
+'''
+i=1
+while i <= 5:
+    print(i)
+    i = i + 1
+'''
+#lisitas
+
+mi_lista = ['Argentina', 200, True]
+            #0,1,2
+mi_lista.append(5000)
+mi_lista.insert(1, "Paises Bajos")
+#for i in mi_lista: #imprime todos los elementos de la lista
+#    print(i)
+#print(mi_lista)
+
+#tuplas
+mi_tupla = ("Celeste", 200, "Rojo")
+#INMUTABLE
+#mi_tupla[1] = 400
+#print(mi_tupla[1])
+#diccionarios dicc
+'''
+persona = {
+    "nombre" : "Jaime",
+    "apellido" : "Desconocido",
+    "edad" : 34
+}
+'''
+'''
+print(persona.keys())
+print(persona.values())
+print(persona.items())
+'''
+
+#Funciones
+'''
+def saludo(nombre):
+    print(f"Hola {nombre}")
+saludo("Nati")
+saludo("Raul")
+'''
+'''
+def saludo(nombre):
+    return f'Hola {nombre}'
+saludo("Nati")
+saludo("Raul")
+print(saludo('Nati'))
+'''
+'''
+#MODULARIZAR
+def sumar(a, b):
+    return a + b
+
+def calculadora_simple(operacion, a, b):
+    if operacion == 'sumar':
+        return sumar(a, b)
+    else:
+        return 'Operacion No Valida'
+print(calculadora_simple("sumar", 7, 3))
+'''
+'''
+try:
+    resultado= 10/0
+except ZeroDivisionError as e:
+    print(f'Error : No se puede dividir entre 0')
+    print(f'Error : {e}')
+'''
+'''
+try:
+    numero = int('abc')
+except ValueError as e:
+    print(f'Error : {e}')
+'''
+
+'''
+persona = {
+    "nombre" : "Jaime",
+    "apellido" : "Desconocido",
+    "edad" : 34
+}
+try:
+    print(persona['documento'])
+except KeyError as e:
+    print('la clave no existe')
+'''
